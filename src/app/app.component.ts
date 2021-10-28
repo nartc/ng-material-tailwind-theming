faa0385f2e191a373c7c879c85c766365a974162
Chau Tran <nartc7789@gmail.com>
2021-10-27 23:21:26 -0500

    theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,11 @@ import { Component } from "@angular/core";
   selector: "app-root",
   template: `
     <button mat-flat-button color="primary">Material Button</button>
+    <button class="px-4 py-2 rounded bg-primary-500 text-white">
+      Tailwind Button
+    </button>
+    <h1 class="text-primary-500">Tailwind Heading One</h1>
+    <div class="h-48 w-48 rounded bg-primary-500"></div>
   `,
   styles: [],
 })
